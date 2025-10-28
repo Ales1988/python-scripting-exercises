5dcb6f98df5c9c56f3eb4a3cfbf8f74283892927
Alessandro Brancolini <xaletherockx@gmail.com>
2025-10-28 19:04:10 +0100

    change password input

--- src/scripts/password_checker/main.py
+++ src/scripts/password_checker/main.py
@@ -1,9 +1,9 @@
 """
-This script checks if and how many times a variable number of passwords has been leaked.
+This script checks if and how many times a password has been leaked.
 Run it in the root of the project using:
-python src/scripts/password_checker/main.py {password} {passwrd} ...
+python src/scripts/password_checker/main.py
 
-The script sends only the first five characters of each password to
+The script sends only the first five characters of the password to
 pwned api. Then it receives all leaked password whose hashes start
 with those characters and uses this response locally to check if input password
 has been leaked. It never sends your entire password on the internet.
@@ -11,15 +11,14 @@ has been leaked. It never sends your entire password on the internet.
 
 import requests
 import hashlib
-import sys
+import getpass
 
 
-# This API receives the first five characters of an SHA-1 hash and
-# returns all leaked password hashes whose hashes start with those characters.
+# The pwned API receives the first five characters of an SHA-1 hash and
+# returns all leaked password whose hashes start with those characters.
 def request_api_data(hash_password):
     url = "https://api.pwnedpasswords.com/range/" + hash_password
     response = requests.get(url)
-    print(response)
     if response.status_code != 200:
         raise RuntimeError(f"Error: {response.status_code}")
     return response
@@ -35,26 +34,19 @@ def get_password_leaks_count(hashes, tail_hash_to_check):
     return 0
 
 
-# Check if password exists in pwned database
+# Hashes the password, call the pwned api, run and return leaks count
 def check_password(password):
-    # Encoding plain password in Sha-1
+    # hashing password in Sha-1
     sha1password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)  # Response->"hash: count, hash: count etc"
     return get_password_leaks_count(response, tail)
 
 
-def main(args):
-    for password in args:
-        count = check_password(password)
-        if count:
-            print(f"{password} was found {count} times. You should change it")
-        else:
-            print(f"{password} was not found. You are safe!")
-    return "Password leak checked!"
-
-
 if __name__ == "__main__":
-    sys.exit(main(sys.argv[1:]))  # take 1 or more arguments.
-    # sys.exit stops the program and passes the return value of main() as the process exit code
-    # to the operative system.
+    password = getpass.getpass("Enter password: ")
+    count = check_password(password)
+    if count:
+        print(f"{password} was found {count} times. You should change it")
+    else:
+        print(f"{password} was not found. You are safe!")
